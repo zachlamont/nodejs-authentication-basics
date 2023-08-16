ed7219b92e5b0144fdb3640e4d0a042b8bb18dcc
zachlamont <zachlamont1@gmail.com>
2023-08-16 21:21:30 +1000

    Complete tutorial

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # nodejs-authentication-basics
+
+A simple Node Express app that demonstrates authorization using Passport.js and securing of passwords with bcrypt
+
 https://www.theodinproject.com/lessons/nodejs-authentication-basics
+http://www.passportjs.org/
+
+To run type npm start and visit http://localhost:3000/
+
+Installations:
+
+1. npm init
+2. npm install express express-session mongoose passport passport-local ejs
---
+++ app.js
@@ -0,0 +1,143 @@
+/////// app.js
+
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const mongoDb =
+  "mongodb+srv://zachlamont1:N61WFQdGQwowdNf0@cluster0.6cwfpik.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+const User = mongoose.model(
+  "User",
+  new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+  })
+);
+
+const app = express();
+app.set("views", __dirname);
+app.set("view engine", "ejs");
+
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
+    try {
+      const user = await User.findOne({ username: username });
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      //if (user.password !== password) {
+      //return done(null, false, { message: "Incorrect password" });
+      // }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        // passwords do not match!
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async function (id, done) {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+}); //If you insert this code somewhere between where you instantiate the passport middleware and before you render your views, you will have access to the currentUser variable in all of your views, and you won’t have to manually pass it into all of the controllers in which you need it.
+
+app.get("/", (req, res) => {
+  res.render("index", { user: req.user });
+});
+
+app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+
+/* 
+app.post("/sign-up", async (req, res, next) => {
+  try {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+    });
+    const result = await user.save();
+    res.redirect("/");
+  } catch (err) {
+    return next(err);
+  }
+}); //this is not a particularly safe way to create users in your database… BUT you should now be able to visit /sign-up, and submit the form.
+
+ */
+
+app.post("/sign-up", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    // Hash the password
+    const saltRounds = 10;
+    bcrypt.hash(password, saltRounds, async (err, hashedPassword) => {
+      if (err) {
+        // Handle the error
+        return next(err);
+      }
+
+      // Create a new user with the hashed password
+      const user = new User({
+        username: username,
+        password: hashedPassword, // Store the hashed password in the database
+      });
+
+      try {
+        // Save the user to the database
+        const result = await user.save();
+        res.redirect("/");
+      } catch (saveError) {
+        return next(saveError);
+      }
+    });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
+app.get("/log-out", (req, res, next) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+app.listen(3000, () => console.log("app listening on port 3000!"));
